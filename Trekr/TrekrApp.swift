8fab257adbed00efed45a4876212e7d80514e902
NexusUA <pbogomolny93@gmal.com>
2023-01-06 18:14:07 +0100

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Pavel Bohomolnyi on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
